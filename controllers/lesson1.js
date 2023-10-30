adca7bb77b306ca464e2b0a8972b08dbd51f3736
Marianaeiterer <marieiterer@gmail.com>
2023-10-30 17:51:43 -0600

    Update

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -2,11 +2,16 @@ const mariRoute = (req, res) => {
     res.send('Mari');
 };
 
+const tannerRoute = (req, res) => {
+    res.send('Tanner Overbay');
+};
+
 const hannaRoute = (req, res) => {
     res.send('Hanna Birch');
 };
 
 module.exports ={
+    tannerRoute,
     mariRoute,
     hannaRoute
 };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router();
 const lession1Controller = require('../controllers/lesson1');
- 
+
+routes.get('/tanner', lession1Controller.tannerRoute); 
 routes.get('/', lession1Controller.mariRoute); 
 routes.get('/hannah', lession1Controller.hannaRoute);
 
